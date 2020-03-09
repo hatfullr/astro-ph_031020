5eaf1e83f5ae94f20a7b9978d0adb610103265df
hatfullr <hatfull@ualberta.ca>
2020-03-09 15:44:21 -0600

    Profiling with line_profiler

---
+++ profiling/numpy_loop.py
@@ -0,0 +1,20 @@
+import numpy as np
+data = np.load("data.npy")
+x = data[:,0] # Use NumPy arrays
+y = data[:,1]
+z = data[:,2]
+N = len(x) # x, y, and z all same length
+
+@profile
+def main():
+    r = np.array([])
+    for i in range(0,N):
+        for j in range(0,N):
+            dx2 = (x[i]-x[j])**2.
+            dy2 = (y[i]-y[j])**2.
+            dz2 = (z[i]-z[j])**2.
+            dr = (dx2 + dy2 + dz2)**0.5
+            r = np.append(r,dr)
+main()
+
+
---
+++ profiling/numpy_loop_4.py
@@ -0,0 +1,20 @@
+import numpy as np
+data = np.load("data.npy")
+x = data[:,0] # Use NumPy arrays
+y = data[:,1]
+z = data[:,2]
+N = len(x) # x, y, and z all same length
+
+@profile
+def main():
+    r = np.zeros(shape=(N,N))
+    for i in range(0,N):
+        dx2 = (x[i]-x)**2.
+        dy2 = (y[i]-y)**2.
+        dz2 = (z[i]-z)**2.
+        dr = (dx2 + dy2 + dz2)**0.5
+        r[i,:] = dr
+        #r[i*N:(i+1)*N] = dr
+main()
+
+
